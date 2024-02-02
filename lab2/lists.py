94b731a23a9ca07abf8a5796e1adbc3503e6489a
terkglva <erkegalievatomiris@gmail.com>
2024-02-02 19:08:09 +0600

    lists update

--- lab2/lists.py
+++ lab2/lists.py
@@ -14,5 +14,5 @@ print(list1)
 mylist = ["apple", "banana", "cherry"]
 print(type(mylist))
 
-thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+thislist = list(("apple", "banana", "cherry")) 
 print(thislist)
