bb57ba516b9737966920f0537ffb3cc3837b61e3
terkglva <erkegalievatomiris@gmail.com>
2024-05-02 03:00:19 +1400

    add pause

--- lab10/snake.py
+++ lab10/snake.py
@@ -16,88 +16,82 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-
-
-# creating a snake class 
 class Snake:
     def __init__(self):
-        self.body = [Vector2(7, 10), Vector2(6, 10), Vector2(5, 10)] # head, and body elements of the snake
-        self.eated = False # checker for snake eated the fruit or not
-        self.isDead = False # check snake dead or not
+        self.body = [Vector2(7, 10), Vector2(6, 10), Vector2(5, 10)] 
+        self.eated = False 
+        self.isDead = False 
 
-    # drawing our snake
+    
     def drawingSnake(self):
-        for block in self.body: # the for loop that iterate our list of cooredinates of snake elements
-            body_rect = pygame.Rect(block.x * cell_size, block.y * cell_size, cell_size, cell_size) # creating rectangle of the snake elements
-            pygame.draw.rect(screen, (0 ,128 ,0), body_rect) # drawing the rectangles
+        for block in self.body: 
+            body_rect = pygame.Rect(block.x * cell_size, block.y * cell_size, cell_size, cell_size) 
+            pygame.draw.rect(screen, (0 ,128 ,0), body_rect)
         snake_head = pygame.Rect(self.body[0].x * cell_size, self.body[0].y * cell_size, cell_size, cell_size)
         headTexture = pygame.image.load('snakehead.png')
         headTexture = pygame.transform.scale(headTexture, (40, 40))
         screen.blit(headTexture, snake_head)
 
-    # the moving of our snake
+    
     def snakeMoving(self):
-        if self.eated == True: # if snake eated the fruit
-            body_copy = self.body[:] # take the copy of the snake
-            body_copy.insert(0, body_copy[0] + direction) # adding the one element
+        if self.eated == True: 
+            body_copy = self.body[:] 
+            body_copy.insert(0, body_copy[0] + direction) 
             self.body = body_copy[:] 
             self.eated = False
         else:
-            body_copy = self.body[:-1] # taking the copy of the snake except the last element
-            body_copy.insert(0, body_copy[0] + direction) # adding one element at index 0 this element is our head + direction
+            body_copy = self.body[:-1] 
+            body_copy.insert(0, body_copy[0] + direction) 
             self.body = body_copy[:]
 
-# creating our Fruit class
+
 class Fruit:
     def __init__(self):
-        self.randomize() # the method that spawns our fruit at the random position
+        self.randomize() 
     
     def drawingFruit(self):
         fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size) 
-        self.food = pygame.image.load(f'food{self.randomFood}.png').convert_alpha() # spawning random fruit
-        self.food = pygame.transform.scale(self.food, (35, 35)) # scale the image
-        # pygame.draw.rect(screen, (107 ,142 ,35), fruit_rect)
+        self.food = pygame.image.load(f'food{self.randomFood}.png').convert_alpha() 
+        self.food = pygame.transform.scale(self.food, (35, 35)) 
+        
         screen.blit(self.food, fruit_rect)
 
     def randomize(self):
-        self.x = random.randint(0, cell_number - 2) # random cell between 0 - 18, whu 18 because if it will be bigger, the fruit will spawn on the borders 
+        self.x = random.randint(0, cell_number - 2) 
         self.y = random.randint(0, cell_number - 2) 
         self.pos = Vector2(self.x, self.y)
-        self.randomFood = random.randint(1, 3) # taking random number between 1-3
+        self.randomFood = random.randint(1, 3) 
 
-# creating the game class that will allow us to control the game
+
 class Game:
     def __init__(self):
-        self.snake = Snake() # creating the snake object
-        self.fruit = Fruit() # creating the fruit object
-        self.level = 1 # our 1st level
-        self.score = 0 # the score, that will increase after eating the fruit
-        # self.isDead = False
+        self.snake = Snake() 
+        self.fruit = Fruit() 
+        self.level = 1 
+        self.score = 0 
 
-    # update method which responsible to snake moving and collision checker
+    
     def update(self):
         self.snake.snakeMoving()
         self.checkCollision()
-        # self.levelAdding()
-        # self.difficulty()
-
+        
     def drawElements(self):
         self.snake.drawingSnake()
         self.fruit.drawingFruit()
         self.scoreDrawing()
     
     def checkCollision(self):
-        if(self.fruit.pos == self.snake.body[0]): # if the coordinates of our snake and fruit will be equal, snake will eat
+        if(self.fruit.pos == self.snake.body[0]): 
             self.snake.eated = True
-            # the 3 types of the food that gives to us, three types of score
+            
             if(self.fruit.randomFood == 1):
                 self.score += 1
             if(self.fruit.randomFood == 2):
                 self.score += 2
             if(self.fruit.randomFood == 3):
                 self.score += 3
-            self.fruit.randomize() # after eating spawning at the random position
-            self.levelAdding() # check for adding level
+            self.fruit.randomize() 
+            self.levelAdding() 
 
         for block in self.snake.body[1:]:
             if block == self.fruit.pos:
@@ -108,8 +102,6 @@ class Game:
                 self.fruit.randomize()
         
 
-
-    # check for our snake collides with borders
     def gameOver(self):
         if self.snake.body[0].x >= 19:
             return True
@@ -120,7 +112,6 @@ class Game:
         if self.snake.body[0].y <= 0:
             return True
         
-        # check for our snake collides with his body
         for block in self.snake.body[1:]:
             if block == self.snake.body[0]:
                 return True
@@ -131,7 +122,7 @@ class Game:
         return False
     
     
-    # check for adding level or not for evety 3 points
+   
     def levelAdding(self):
         global snake_speed
         if self.score // 3 > self.level:
@@ -139,7 +130,7 @@ class Game:
             snake_speed -= 10
             pygame.time.set_timer(SCREEN_UPDATE, snake_speed) # increasing speed
     
-    # drawing the UI of our game, such us score and level
+    
     def scoreDrawing(self):
         score_text = "Score: " + str(self.score)
         score_surface = font.render(score_text, True, (56, 74, 12))
@@ -201,11 +192,15 @@ class Game:
                 wall_rect = pygame.Rect(wall * cell_size, 16 * cell_size, cell_size, cell_size)
                 screen.blit(wall_texture, wall_rect)
                 wall_coordinates.append(Vector2(wall, 16))
-
+                
+        
     def pauseState(self):
         global isPause
         if isPause == False:
-            screen.blit(pause, (0, 0, 800, 800))
+            pause_image = pygame.image.load('pause.png') 
+            pause_rect = pause.get_rect(center=(cell_size * cell_number // 2, cell_size * cell_number // 2))
+            screen.blit(pause, pause_rect)
+            pygame.display.flip()
             pygame.time.set_timer(SCREEN_UPDATE, 0)
             isPause = True
         else:
@@ -216,22 +211,22 @@ class Game:
 clock = pygame.time.Clock()
 cell_size = 40
 cell_number = 20
-direction = Vector2(1, 0) # direction this means to right
+direction = Vector2(1, 0) 
 screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number)) # creating screen with are 800x800 or 20x20 cells square
 done = False
 
 
-font = pygame.font.Font('font.ttf', 25) # imporing our font, from our file
+font = pygame.font.Font('font.ttf', 25) 
 
 nowSeconds = int((datetime.datetime.now()).strftime("%S"))
 
-game = Game() # creating the game object
+game = Game() 
 snake_speed = 150
 
 wall1 = [Vector2(9, 8), Vector2(9, 9), Vector2(9, 10), Vector2(9, 11)]
 wall_texture = pygame.image.load('cobble4040.png')
 wall_coordinates = []
-pause = pygame.image.load('pause.png')
+pause = pygame.image.load('/Users/Владелец/Desktop/q/lab10/pause.png')
 
 isPause = False
 
@@ -264,10 +259,11 @@ def start_the_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-            # check for direction
+           
             if(event.type == SCREEN_UPDATE):
                 game.update()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game.pauseState()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
                 if direction.x != -1:
@@ -289,7 +285,7 @@ def start_the_game():
                 conn.close()
                 done = True
 
-        if(game.gameOver() == True): # if our game over returns true, we will end the game
+        if(game.gameOver() == True):
 
             cur.execute(f''' INSERT INTO snake_score("username", "user_score") VALUES( '{user_name}', '{game.score}'); ''')
 
@@ -298,7 +294,7 @@ def start_the_game():
             conn.close()
             done = True
 
-        # check for to randomly spawn the fruit, if you wont eat the frut after 3 seconds it will be spawned at the random position
+    
         time = datetime.datetime.now()
         seconds = int(time.strftime("%S"))
         if abs(seconds - nowSeconds) > 3:
@@ -307,7 +303,6 @@ def start_the_game():
         screen.fill((175, 215, 70))
         game.spawingWalls()
         game.drawElements()
-        # game.update()
         pygame.display.flip()
         clock.tick(60)
 
@@ -321,5 +316,4 @@ menu.add.button('Play', name_checker, name_box)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 menu.mainloop(screen)
 
-# start_the_game()
 pygame.quit()
